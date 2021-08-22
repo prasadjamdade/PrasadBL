e5256deeecd7048c3ba0967b163063ef0abc4c67
prasadjamdade <prasadjamdade@hotmail.com>
2021-08-22 21:16:18 +0530

    Problem 1: START

---
+++ start.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo "                                                             ";
+echo "=============================================================";
+echo "Welcome to Employee Wage Computation Program on Master Branch";
+echo "=============================================================";
+echo "                                                             ";
