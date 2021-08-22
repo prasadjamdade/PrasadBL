8339146fa20842f01c7cf2546613e4a357ead9a4
prasadjamdade <prasadjamdade@hotmail.com>
2021-08-22 21:27:43 +0530

    code for full time pay declaration

---
+++ Dailywage.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "                                                    ";
+echo "====================================================";
+echo "This is just the declaration sheet for full time Emp";
+echo "====================================================";
+echo "                                                    ";
+
+echo "Daily wage for an average employe is $((20 * 8))";
+echo "Assuming the Employee worked for 8 hours";
+echo "And per hour pay for Employee is 20 bucks";
--- Empattendance.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-random=$((RANDOM%2))
-if [[ $random == 0 ]]; 
-then
-	echo "Employee is present";
-else
-	echo "Employee is absent";
-fi
