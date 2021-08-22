4951ae2eb365cf8d44ac04090c4e2f846204bc3d
prasadjamdade <prasadjamdade@hotmail.com>
2021-08-22 22:58:19 +0530

    Display of declaration of part time pay

--- Dailywagepart.sh
+++ Dailywagepart.sh
@@ -2,10 +2,10 @@
 
 echo "                                                    ";
 echo "====================================================";
-echo "This is just the declaration sheet for full time Emp";
+echo "This is just the declaration sheet for part time Emp";
 echo "====================================================";
 echo "                                                    ";
 
-echo "Daily wage for an average employe is $((20 * 8))";
-echo "Assuming the Employee worked for 8 hours";
+echo "Daily wage for an average employe is $((20 * 8 / 2)) bucks";
+echo "Assuming the Employee worked for 4 hours as half day";
 echo "And per hour pay for Employee is 20 bucks";
