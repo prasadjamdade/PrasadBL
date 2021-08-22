223c19aa7b136a99ee6c447b64df6b71bf8a6512
prasadjamdade <prasadjamdade@hotmail.com>
2021-08-22 21:22:51 +0530

    deleted content of START

--- start.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-echo "                                                             ";
-echo "=============================================================";
-echo "Welcome to Employee Wage Computation Program on Master Branch";
-echo "=============================================================";
-echo "                                                             ";
